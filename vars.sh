4325c0bbb596407020353149c66c55f65584a76a
igor725 <gvaldovigor@gmail.com>
2022-02-19 18:23:55 +0300

    Fix vars

--- vars.sh
+++ vars.sh
@@ -1,14 +1,17 @@
-LUA_LIB=$(pkg-config --libs luajit)
-if [ $? -ne 0 ]; then
-	LUA_LIB=$(pkg-config --libs lua)
-	if [ $? -ne 0 ]; then
+LJNAME="luajit >= 2.0"
+LNAME="lua >= 5.1"
+
+if pkg-config --exists $LJNAME; then
+	LUA_LIB=$(pkg-config --libs $LJNAME)
+	CFLAGS="$CFLAGS $(pkg-config --cflags $LJNAME)"
+else
+	if pkg-config --exists --print-errors $LNAME; then
+		LUA_LIB=$(pkg-config --libs $LNAME)
+		CFLAGS="$CFLAGS $(pkg-config --cflags $LNAME)"
+	else
 		echo "Lua not found"
 		exit 1
-	else
-		CFLAGS="$CFLAGS $(pkg-config --cflags lua)"
 	fi
-else
-	CFLAGS="$CFLAGS $(pkg-config --cflags luajit)"
 fi
 
 LIBS="$LIBS $LUA_LIB"
