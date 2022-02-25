ecf78a53d65c4b9487210559bcd19b1fcd52aff3
igor725 <gvaldovigor@gmail.com>
2022-02-25 14:29:57 +0300

    Fix log's pushfmt

--- src/lualog.c
+++ src/lualog.c
@@ -4,20 +4,17 @@
 
 static void pushfmt(lua_State *L) {
 	int count = lua_gettop(L);
-	lua_getglobal(L, LUA_STRLIBNAME);
-	lua_getfield(L, -1, "format");
-	lua_remove(L, -2);
-	lua_getglobal(L, "tostring");
-	if(count > 0)
+	if(count > 0) {
+		lua_getglobal(L, LUA_STRLIBNAME);
+		lua_getfield(L, -1, "format");
+		lua_remove(L, -2);
+		lua_getglobal(L, "tostring");
 		lua_pushvalue(L, 1);
-	else {
-		lua_pushnil(L);
-		count++;
-	}
-	lua_call(L, 1, 1);
-	for(int i = 2; i <= count; i++)
-		lua_pushvalue(L, i);
-	lua_call(L, count, 1);
+		lua_call(L, 1, 1);
+		for(int i = 2; i <= count; i++)
+			lua_pushvalue(L, i);
+		lua_call(L, count, 1);
+	} else lua_pushstring(L, "nil");
 }
 
 static int log_info(lua_State *L) {
