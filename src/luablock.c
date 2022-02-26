73aab3641e9ca51457071b709645a2a8032e4575
igor725 <gvaldovigor@gmail.com>
2022-02-26 08:04:49 +0300

    block.isvalid added

--- src/luablock.c
+++ src/luablock.c
@@ -5,19 +5,37 @@
 #include "luaworld.h"
 #include "luablock.h"
 
+static const luaL_Reg blockmeta[] = {
+	{NULL, NULL}
+};
+
 static int block_define(lua_State *L) {
 	luaL_checktype(L, 1, LUA_TTABLE);
 	
 	return 0;
 }
 
+static int block_isvalid(lua_State *L) {
+	World *world = lua_checkworld(L, 1);
+	BlockID id = (BlockID)luaL_checkinteger(L, 2);
+	lua_pushboolean(L, Block_IsValid(world, id));
+	return 1;
+}
+
 static const luaL_Reg blocklib[] = {
 	{"define", block_define},
+	{"isvalid", block_isvalid},
 
 	{NULL, NULL}
 };
 
 int luaopen_block(lua_State *L) {
+	luaL_newmetatable(L, "Block");
+	lua_pushvalue(L, -1);
+	lua_setfield(L, -2, "__index");
+	luaL_setfuncs(L, blockmeta, 0);
+	lua_pop(L, 1);
+
 	luaL_register(L, luaL_checkstring(L, 1), blocklib);
 	return 1;
 }
