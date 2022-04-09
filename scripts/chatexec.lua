5089f8cdb9074e7a5d62c28e019ff2aae4b3376f
igor725 <gvaldovigor@gmail.com>
2022-04-09 13:24:29 +0300

    /luarun command added

--- scripts/chatexec.lua
+++ scripts/chatexec.lua
@@ -1,5 +1,39 @@
 allowHotReload(true)
 
+local function runScript(cl, script)
+	if not script or #script == 0 then
+		return '&cTrying to execute empty string'
+	end
+
+	_G.self = cl
+	local chunk, err = load(script, 'chatexec')
+
+	if chunk then
+		if cl then
+			log.warn('%s executed lua script: %s', cl:getname(), script)
+		end
+
+		local out = {pcall(chunk)}
+		_G.self = nil
+
+		if not table.remove(out, 1) then
+			return ('&cRuntime error: %s'):format(out[1])
+		end
+
+		for i = 1, #out do
+			out[i] = tostring(out[i])
+		end
+
+		if #out > 0 then
+			return '&aOutput&f: ' .. table.concat(out, ' ')
+		end
+
+		return '&aScript executed successfully'
+	else
+		return ('&cSyntax error: %s'):format(err)
+	end
+end
+
 function onStart()
 	setmetatable(_G, {
 		__index = function(self, key)
@@ -8,46 +42,31 @@ function onStart()
 			return rawget(self, key)
 		end
 	})
+
+	command.add('luarun', 'Execute Lua script', CMDF_OP, runScript)
 end
 
 function onMessage(cl, type, text)
-	if cl:isop() then
-		if text:find('^>') then
-			text = text:gsub('^(>=?)', function(s)
-				if s == '>=' then
-					return 'return '
-				end
+	if not cl:isop() then
+		return true
+	end
 
-				return ''
-			end)
-
-			_G.self = cl
-			local chunk, err = load(text, 'chatexec')
-
-			if chunk then
-				log.warn('%s executed lua script: %s', cl:getname(), text)
-				local out = {pcall(chunk)}
-				_G.self = nil
-
-				if not table.remove(out, 1) then
-					cl:chat(('&cRuntime error: %s'):format(out[1]))
-					return false
-				end
-
-				for i = 1, #out do
-					out[i] = tostring(out[i])
-				end
-
-				if #out > 0 then
-					cl:chat('&aOutput&f: ' .. table.concat(out, ' '))
-				else
-					cl:chat('&aScript executed successfully')
-				end
-			else
-				cl:chat(('&cSyntax error: %s'):format(err))
+	if text:find('^>') then
+		text = text:gsub('^(>=?)', function(s)
+			if s == '>=' then
+				return 'return '
 			end
 
-			return false
-		end
+			return ''
+		end)
+		
+		cl:chat(runScript(cl, text))
+		return false
 	end
 end
+
+function onStop()
+	command.remove('luarun')
+end
+
+preReload = onStop
