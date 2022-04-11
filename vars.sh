98467278ec53d4b94f0edbf3c08dd95d5cf271ec
igor725 <gvaldovigor@gmail.com>
2022-04-12 01:32:01 +0300

    Cross compiling fix

--- vars.sh
+++ vars.sh
@@ -1,13 +1,14 @@
 LJNAME="luajit >= 2.0"
 LNAME="lua >= 5.1"
+PKCFG=$MACH-pkg-config
 
-if pkg-config --exists $LJNAME; then
-	LUA_LIB=$(pkg-config --libs $LJNAME)
-	CFLAGS="$CFLAGS $(pkg-config --cflags $LJNAME)"
+if $PKCFG --exists $LJNAME; then
+	LUA_LIB=$($PKCFG --libs $LJNAME)
+	CFLAGS="$CFLAGS $($PKCFG --cflags $LJNAME)"
 else
-	if pkg-config --exists $LNAME; then
-		LUA_LIB=$(pkg-config --libs $LNAME)
-		CFLAGS="$CFLAGS $(pkg-config --cflags $LNAME)"
+	if $PKCFG --exists $LNAME; then
+		LUA_LIB=$($PKCFG --libs $LNAME)
+		CFLAGS="$CFLAGS $($PKCFG --cflags $LNAME)"
 	else
 		echo "pkg-config: failed to find luajit >= 2.0 or lua >= 5.1"
 		exit 1
